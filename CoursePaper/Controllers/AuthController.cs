fe9beeb4d5f55147137facf7250cdbdbb4c94e5c
xXXTylerDurdenXXx <143999505+xXXTylerDurdenXXx@users.noreply.github.com>
2025-11-27 23:50:36 +0300

    refactor: login.html => Login.cshtml, refactor: register => Register.cshtml. feat: add AuthController feat: add additional navigation buttons

---
+++ CoursePaper/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoursePaper.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            // TODO: логика проверки пользователя
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Register(string name, string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                // регистрация пользователя
+            }
+            return RedirectToAction("Register", "Auth");
+        }
+
+        
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            // выход пользователя
+            return RedirectToAction("Login");
+        }
+    }
+}
